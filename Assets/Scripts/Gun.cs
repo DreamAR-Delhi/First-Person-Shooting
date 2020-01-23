3e35d7ca736d2e09e4b1c7bcf8772052f8d699ca
ColossalMonk <17siddhartha.satyarthi@gmail.com>
2020-01-23 21:57:20 +0530

    Add files via upload

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 0f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+    public GameObject Bullet_Emitter;
+    public GameObject Bullet;
+    public float Bullet_Forward_Force;
+ 
+ 
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.takeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet,Bullet_Emitter.transform.position,Bullet_Emitter.transform.rotation) as GameObject;
+
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+            Destroy(Temporary_Bullet_Handler, 5.0f);
+
+        }
+    }
+}
